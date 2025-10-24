b8f8638a11025e24e870ce9d76ada90c8b8d7b05
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2025-10-23 22:40:44 -0700

    Been slow with this course but we're making progress regardles

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,30 +1,33 @@
 package com.example.helloworld
 
-import android.graphics.ColorSpace
 import android.os.Bundle
-import android.provider.CalendarContract
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
-import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -32,14 +35,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.helloworld.ui.theme.HelloWorldTheme
 
 class MainActivity : ComponentActivity() {
@@ -50,8 +50,9 @@ class MainActivity : ComponentActivity() {
             HelloWorldTheme {
 
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    var text by remember { mutableStateOf("") }
-                    StateLessGreeting(modifier = Modifier.padding(innerPadding), text = text, onTextChange = { text = it })
+                    Example(
+                        modifier = Modifier.padding(innerPadding),
+                    )
                 }
             }
         }
@@ -59,55 +60,55 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ComposableWithState(modifier: Modifier = Modifier) {
-    var text by remember { mutableStateOf("") }
+fun Example(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    var progress by remember { mutableFloatStateOf(0.1f) }
     Column (
-        modifier = modifier
-    ) {
-        TextField(
-            value = text,
-            onValueChange = { text = it },
-            label = { Text("Enter your name") }
-        )
-        Text("Text is: $text")
-    }
-}
-
-@Composable
-fun StateLessGreeting(modifier: Modifier = Modifier, text: String, onTextChange: (String) -> Unit) {
-    Column (
-        modifier = modifier
-    ) {
-        TextField(
-            value = text,
-            onValueChange = onTextChange,
-            label = { Text("Enter your name") }
-        )
-        Text("Text is: $text")
-    }
-}
-
-@Composable
-fun HelloWorld(modifier: Modifier) {
-    // "remember" tells compose to remember this value across recompositions
-    var count by remember { mutableIntStateOf(value = 0) }
-    Column (
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier.fillMaxSize().background(color = Color.White)
     ) {
-        Text("Count is: $count")
-        Button(
-            onClick = {
-                count++
-                Log.v("Dummy Logs", "Count is $count")
-            }
+        LinearProgressIndicator(
+        progress = { progress },
+        modifier = Modifier.padding(10.dp).height(8.dp) ,
+        color = Color.Red,
+        )
+
+        Row (
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
         ) {
-            Text("increase count")
+            Button(
+                onClick = {
+                    if (progress < 1) {
+                        progress += 0.1f
+                    } else {
+                        Toast.makeText(context, "Progress cannot go any higher than $progress", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            ) {
+                Text("Add")
+            }
+
+            Spacer(Modifier.width(10.dp))
+            Button(
+                onClick = {
+                    if (progress  > 0.0f) {
+                        progress -= 0.1f
+                    } else {
+                        Toast.makeText(context, "Progress cannot go any lower than $progress", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            ) {
+                Text("Minus")
+            }
         }
+
     }
 }
 
 @Preview
 @Composable
-fun HelloWorldExample() {
-    HelloWorld(Modifier)
+fun ExamplePreview() {
+    Example()
 }
