859a948ce42fdd67be629cd827411791c36c0830
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2025-10-26 15:47:56 -0700

    fillMaxWidth() plus ContentScale.Fit seems to be the best way to maintain aspect ratio while retaining as much of the image as possible

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -5,6 +5,7 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -16,6 +17,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -25,7 +28,6 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -35,6 +37,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -98,7 +101,7 @@ class MainActivity : ComponentActivity() {
                         }
                     },
                     modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    BoxExample(
+                    ScrollViewExample(
                         modifier = Modifier.padding(innerPadding),
                     )
                 }
@@ -179,6 +182,19 @@ fun BoxExample(modifier: Modifier = Modifier) {
                 color = Color.White, fontWeight = FontWeight.Bold, fontSize = 24.sp
             )
         }
+    }
+}
+
+@Composable
+fun ScrollViewExample(modifier: Modifier = Modifier) {
+    Column (
+        modifier = modifier.verticalScroll(rememberScrollState())
+    ) {
+        Image(painter = painterResource(R.drawable.elephant), contentDescription = "elephant", modifier = Modifier.fillMaxWidth(), contentScale = ContentScale.Fit)
+        Image(painter = painterResource(R.drawable.elephant), contentDescription = "elephant", modifier = Modifier.fillMaxWidth(), contentScale = ContentScale.Fit)
+        Image(painter = painterResource(R.drawable.elephant), contentDescription = "elephant", modifier = Modifier.fillMaxWidth(), contentScale = ContentScale.Fit)
+        Image(painter = painterResource(R.drawable.elephant), contentDescription = "elephant", modifier = Modifier.fillMaxWidth(), contentScale = ContentScale.Fit)
+        Image(painter = painterResource(R.drawable.elephant), contentDescription = "elephant", modifier = Modifier.fillMaxWidth(), contentScale = ContentScale.Fit)
     }
 }
 
